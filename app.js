2eeb6a9ebd625f7fc84fc2b954368a98f191805c
EthanyenTMC <ethandoescoding@gmail.com>
2021-07-19 01:13:14 -0400

    thing

---
+++ app.js
@@ -0,0 +1,33 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+
+const section = document.querySelector('section');
+const end = section.querySelector('h1');
+
+//scroll stuff
+const controller = new ScrollMagic.Controller();
+//scene
+const scene = new ScrollMagic.Scene({
+    duration: 3500,
+    triggerElemnt: intro,
+    triggerHook: 0
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
+
+
+//video animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on('update', e => {
+    scrollpos = e.scrollPos / 1000; //scrollPos is scrollMagic thing not my variable DO NOT CONFUSE
+});
+
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+
+    video.currentTime = delay;
+}, 16.6)
