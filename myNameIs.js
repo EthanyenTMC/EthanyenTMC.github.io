3550bcad12ff623af81ef45b0518dd7a1e4d8140
EthanyenTMC <68873703+EthanyenTMC@users.noreply.github.com>
2021-10-14 22:31:25 -0400

    yeah

--- myNameIs.js
+++ myNameIs.js
@@ -1,19 +1,4 @@
-var navBar = document.getElementById('navBar');
 
-//navbar
-document.onmousemove=getCursorPos;
-var output = document.getElementById('output');
-function getCursorPos(a)
-{
-var posx = a.clientX;
-var posy = a.clientY;
-//output.innerHTML="Position X: " + posx + "px & Position Y: " + posy + "px";
-if(posy < 300){
-        navBar.style.opacity = (300-posy)/200;
-    }else{
-        navBar.style.opacity = 0;
-    }
-} 
 
 //font switch
 var fonts = ["Dancing Script", "Lobster Two", "Rajdhani", "Oswald"];
