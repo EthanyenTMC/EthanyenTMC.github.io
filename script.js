15f9e001be34f767e6d9cd2adc4c00ffcf1631dd
EthanyenTMC <68873703+EthanyenTMC@users.noreply.github.com>
2022-04-13 21:14:50 -0400

    orbit

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
+import * as THREE from 'https://cdn.skypack.dev/three';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
 const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
 
-			const geometry = new THREE.BoxGeometry();
-			const material = new THREE.MeshBasicMaterial( { color: 0x9A6DFF } );
+const controls = new OrbitControls( camera, renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 			const cube = new THREE.Mesh( geometry, material );
 			scene.add( cube );
 
-			camera.position.z = 10;
+//controls.update() must be called after any manual changes to the camera's transform
+camera.position.set( 0, 5, 0 );
+controls.update();
 
-			function animate() {
-				requestAnimationFrame( animate );
+function animate() {
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
+	requestAnimationFrame( animate );
 
-				renderer.render( scene, camera );
-			};
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
 
-			animate();
+	renderer.render( scene, camera );
+
+}
+
+window.onload(animate());
